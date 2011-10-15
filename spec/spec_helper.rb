b508e8adad298b8f06a4a0951058d937e9f00b06
NgTzeYang <ngty77@gmail.com>
2011-10-16 01:06:31 +0800

    Added implementation for "having_source" matcher.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,21 @@
 require 'rubygems'
 require File.expand_path('../../lib/sourcify', __FILE__)
 
+# /////////////////////////////////////////////////////
 # Bacon
+# /////////////////////////////////////////////////////
 require 'bacon'
 Bacon.extend(Bacon::TestUnitOutput)
 Bacon.summary_on_exit
+
+# /////////////////////////////////////////////////////
+# Matchers
+# /////////////////////////////////////////////////////
+def having_source(source)
+  # NOTE: We ignore code indentation while doing checks
+  lines = source.strip.split("\n").map { |s| s.sub(/^\s*/, '') }
+  expected = %r(^#{lines.join("\n\s*")}$)
+
+  lambda { |thing| thing.to_source =~ expected }
+end
+
